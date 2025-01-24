73d147d48d575fda80c2d6f550913d95e96107d6
Sipurit <gun0820303553@gmail.com>
2025-01-24 16:32:30 +0700

    #

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -1,23 +1,55 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
-
-void findLocalMax(const double [][N], bool [][N]);
-
-void showMatrix(const bool [][N]);
-
-int main(){
-	double A[N][N]; 
-	bool B[N][N]; 
-	cout << "Input the matrix.....\n";
-	inputMatrix(A);
-	findLocalMax(A,B);
-	cout << "Result is \n";
-	showMatrix(B);
-	return 0;
+void inputMatrix(double A[N][N]) {
+    for (int i = 0; i < N; i++) {
+        cout << "Row " << i+1 << ": ";
+        for (int j = 0; j < N; j++) {
+            cin >> A[i][j];
+        }
+    }
 }
 
-//Do not modify source code above this line 
+void findLocalMax(const double A[N][N], bool B[N][N]) {
+    // ตั้งค่าขอบของ B ให้เป็น false
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
+                B[i][j] = false; // ขอบเมทริกซ์
+            } else {
+                // ตรวจสอบค่ากับจุดรอบข้าง (บน, ล่าง, ซ้าย, ขวา)
+                if (A[i][j] >= A[i - 1][j] && // ด้านบน
+                    A[i][j] >= A[i + 1][j] && // ด้านล่าง
+                    A[i][j] >= A[i][j - 1] && // ด้านซ้าย
+                    A[i][j] >= A[i][j + 1]) { // ด้านขวา
+                    B[i][j] = true;
+                } else {
+                    B[i][j] = false;
+                }
+            }
+        }
+    }
+}
+
+
+void showMatrix(const bool B[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    double A[N][N];
+    bool B[N][N];
+    cout << "Input the matrix.....\n";
+    inputMatrix(A);
+    findLocalMax(A, B);
+    cout << "Result is \n";
+    showMatrix(B);
+    return 0;
+}
